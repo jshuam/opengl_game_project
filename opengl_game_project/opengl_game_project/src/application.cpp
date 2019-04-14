bc612b4688ee56c462f602576069a90e89f6a10d
Joshua Mayers <jmm450@uowmail.edu.au>
2019-04-14 11:02:32 +1000

    Changed include/lib directories so that they are included in the repo.

--- opengl_game_project/opengl_game_project/src/application.cpp
+++ opengl_game_project/opengl_game_project/src/application.cpp
@@ -42,6 +42,8 @@ int main( void )
 	glGenBuffers( 1, &buffer );
 	glBindBuffer( GL_ARRAY_BUFFER, buffer );
 	glBufferData( GL_ARRAY_BUFFER, 6 * sizeof( float ), positions, GL_STATIC_DRAW );
+	glVertexAttribPointer( 0, 2, GL_FLOAT, GL_FALSE, sizeof( float ) * 2, 0 );
+	glEnableVertexAttribArray( 0 );
 
 	/* Loop until the user closes the window */
 	while( !glfwWindowShouldClose( window ) )
