1707ddecf0be53eee80594b1002b23a445d4e46d
Joshua Mayers <jshuam0@gmail.com>
2019-06-24 22:34:44 +1000

    CMake & Various Changes
    - Got CMake working
    - Fixing GCC compiler errors (worked on MSVC compiler)
    - Added a .gitignore file
    
    Signed-off-by: Joshua Mayers <jshuam0@gmail.com>

--- build/gl/CMakeFiles/gl.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libgl.pdb"
-  "libgl.a"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/gl.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
